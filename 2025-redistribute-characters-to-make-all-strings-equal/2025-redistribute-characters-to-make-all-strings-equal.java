35d5bd06f16529b47448444506119feb34c6db72
Aditya Mapari <adityamapari13@gmail.com>
2025-02-15 13:34:27 +0530

    Time: 2 ms (95.64%), Space: 42.7 MB (92.99%) - LeetHub

---
+++ 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean makeEqual(String[] words) 
+    {
+        int [] freq = new int[26];
+
+        for(int i = 0; i<words.length; i++)
+        {
+            String w = words[i];
+
+            for(int j = 0; j<w.length(); j++)
+            {
+                char ch = w.charAt(j);
+                int indx = (int)(ch - 'a');
+
+                freq[indx] = freq[indx] + 1;
+
+            }
+        }
+
+        for(int i = 0; i<26; i++)
+        {
+            if(freq[i] % words.length != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
