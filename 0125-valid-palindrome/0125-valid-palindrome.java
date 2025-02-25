eff656705b369fa171bfff6ba7b31bc46b72398e
Aditya Mapari <adityamapari13@gmail.com>
2025-02-25 20:35:07 +0530

    Time: 3 ms (74.9%), Space: 43.2 MB (70.89%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -6,34 +6,31 @@ class Solution {
             return true;
         }
 
-       StringBuilder sb = new StringBuilder("");
-
+        StringBuilder sb = new StringBuilder("");
         for(int i = 0; i<s.length(); i++)
         {
             char ch = s.charAt(i);
-
             if(ch>='a' && ch<='z')
             {
                 sb.append(ch);
             }
-            else if(ch>='A' && ch<='Z')
+            else if (ch>='A' && ch<='Z')
             {
-                ch = (char)(ch+32);
+                ch = (char)(ch + 32);
                 sb.append(ch);
             }
-            else if(ch >='0' && ch<='9')
+            else if(ch>='0' && ch<='9')
             {
                 sb.append(ch);
             }
         }
+        
 
-        int start = 0;
-        int end = sb.length() -1;
+        int start  = 0;
+        int end = sb.length()-1;
 
-        while(start < end)
+        while(start <= end)
         {
-            
-
             if(sb.charAt(start) != sb.charAt(end))
             {
                 return false;
@@ -41,6 +38,8 @@ class Solution {
             start++;
             end--;
         }
+        
+
         return true;
     }
 }
