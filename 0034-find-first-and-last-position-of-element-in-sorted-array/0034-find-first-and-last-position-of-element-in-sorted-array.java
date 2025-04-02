a4835839fbbcf1878fa71953d3e343c9c6003c24
Aditya Mapari <adityamapari13@gmail.com>
2025-04-03 00:30:53 +0530

    Time: 0 ms (100%), Space: 45.8 MB (93.21%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,75 +1,59 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] arr = {-1, -1}; 
+    public int[] searchRange(int[] nums, int target) 
+    {
+        int []arr = {-1, -1};
+
         if(nums.length == 0)
         {
             return arr;
         }
 
-        arr[0] = firstOccurence(nums, target);
-        arr[1] = lastOccurence(nums, target);
+        int start = 0, end = nums.length-1;
+
+        //first occurence
+        while(start <= end)
+        {
+            int mid = (start + end)/2;
+
+            if(nums[mid] == target)
+            {
+                arr[0] = mid;
+                end = mid-1;
+            }
+
+           else if(nums[mid] > target)
+            {
+                end = mid-1;
+            }
+            else
+            {
+                start = mid+1;
+            }
+        }
+
+        // last occurence
+        start = 0; end = nums.length-1;
+        while(start <= end)
+        {
+            int mid = (start + end)/2;
+
+            if(nums[mid] == target)
+            {
+                arr[1] = mid;
+                start = mid+1;
+            }
+           else if(nums[mid] > target)
+            {
+                end = mid - 1;
+            }
+
+            else
+            {
+                start = mid + 1;
+            }
+        }
 
         return arr;
-    }
-
-    public int firstOccurence(int[] nums, int target)
-    {
-        int ans = -1;
-        int iStart = 0;
-        int iEnd = nums.length-1;
-
-
-        while(iStart <= iEnd)
-        {
-            int iMid = (iStart + iEnd) / 2;
-
-            if(nums[iMid] == target)
-            {
-                ans = iMid;
-                iEnd = iMid - 1;
-            }
-            else if (target > nums[iMid])
-            {
-                iStart = iMid + 1;
-            }
-            else
-            {
-                iEnd = iMid - 1;
-            }
-           
-        }
-        return ans;
-
-    }
-
-    public int lastOccurence(int[] nums, int target)
-    {
-        int ans = -1;
-        int iStart = 0;
-        int iEnd = nums.length-1;
-
-
-        while(iStart <= iEnd)
-        {
-            int iMid = (iStart + iEnd) / 2;
-
-            if(nums[iMid] == target)
-            {
-                ans = iMid;
-                iStart = iMid + 1;
-            }
-            else if (target > nums[iMid])
-            {
-                iStart = iMid + 1;
-            }
-            else
-            {
-                iEnd = iMid - 1;
-            }
-
-        }
-        return ans;
-
     }
 
        
