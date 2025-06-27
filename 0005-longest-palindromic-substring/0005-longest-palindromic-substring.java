cbc808ae0e02b4d66a5a914f86bcb6f6639d0ea7
Aditya Mapari <adityamapari13@gmail.com>
2025-06-27 17:45:02 +0530

    Time: 2559 ms (5.01%), Space: 45.4 MB (33.6%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,9 +1,13 @@
 class Solution {
-    public Boolean isPalindrome(String s)
-    {
+   public boolean isPalindrome(String s)
+   {
+        if(s.length() == 0 || s.length() == 1)
+        {
+            return true;
+        }
         int left = 0;
         int right = s.length()-1;
-        
+
         while(left <= right)
         {
             if(s.charAt(left) != s.charAt(right))
@@ -14,28 +18,33 @@ class Solution {
             right--;
         }
         return true;
-    }
+   }
     public String longestPalindrome(String s) 
     {
-        String longest = "";
-
         
-
-        for(int i = 0; i<s.length(); i++)
+        if(s.length() == 0 || s.length() == 1)
         {
-            for(int j= i; j<s.length(); j++)
+           return s.substring(0, 1);
+        }
+        String Longest = "";
+       
+       for(int i = 0; i<s.length(); i++)
+       {
+            
+            
+            for(int j = i; j<s.length(); j++)
             {
-                String sub = s.substring(i, j+1);
+                
+                String sub = s.substring(i,j+1);
 
-
-                if(isPalindrome(sub) && sub.length() > longest.length())
+                if(isPalindrome(sub) && sub.length() > Longest.length() )
                 {
-                    longest = sub;
+                    Longest = sub;
                 }
             }
-        }
-        return longest;
-
+       }
+      
+       return Longest;
 
     }
 }
