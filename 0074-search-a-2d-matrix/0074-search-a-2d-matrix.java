cfe7338ff65eb4dcb35794b9618e230345c18c10
Aditya Mapari <adityamapari13@gmail.com>
2025-08-08 23:31:13 +0530

    Time: 0 ms (100%), Space: 42.4 MB (27.25%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) 
     {
-       int row = 0;
-       int col = matrix[0].length - 1;
+      int row = 0;
+      int col = matrix[0].length-1;
 
-       while(row < matrix.length && col >=0)
-       {
+      while(row < matrix.length && col >= 0)
+      {
             if(matrix[row][col] == target)
             {
                 return true;
@@ -18,8 +18,8 @@ class Solution {
             {
                 row++;
             }
-       }
-       return false;
+      }
+      return false;
         
     }
 }
