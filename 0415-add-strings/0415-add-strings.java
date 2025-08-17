070e256b61d72197f90918de56408173cae1faee
Aditya Mapari <adityamapari13@gmail.com>
2025-08-17 20:20:32 +0530

    Time: 4 ms (30.43%), Space: 44.4 MB (11.54%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,29 +1,51 @@
 class Solution {
     public String addStrings(String num1, String num2) {
+        
+        int i = num1.length()-1;
+        int j = num2.length()-1;
         StringBuilder sb = new StringBuilder();
+
         int carry = 0;
-        int a1 = num1.length() - 1;
-        int b1 = num2.length() - 1;
 
-        while (a1 >= 0 || b1 >= 0) {
-            int digit1 = (a1 >= 0) ? num1.charAt(a1) - '0' : 0;
-            int digit2 = (b1 >= 0) ? num2.charAt(b1) - '0' : 0;
+        while(i >= 0 || j >= 0)
+        {
+            int nui = 0;
+            int nuj= 0;
+            
+            if(i >= 0)
+            {
+                nui = num1.charAt(i) - '0';
+            }
+            else
+            {
+                 nui = 0;
+            }
+            if(j >= 0)
+            {
+                nuj= num2.charAt(j) - '0';
+            }
+            else
+            {
+                nuj = 0;
+            }
+              
 
-            int sum = digit1 + digit2 + carry;
-            carry = sum / 10;
-            int cal = sum % 10;
+            int sum = nui + nuj+ carry;
+            sb.insert(0, sum%10);
+             carry = sum/10;
 
-            sb.insert(0, cal);
+            i--;
+            j--;
 
-            a1--;
-            b1--;
+
         }
-
-        if (carry > 0) {
-            sb.insert(0, carry);
+        if(carry > 0)
+        {
+            sb.insert(0,carry);
         }
-
+        
         return sb.toString();
+
 
     }
 }
