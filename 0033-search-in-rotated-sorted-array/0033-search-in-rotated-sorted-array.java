f61744652435ce7f95411a2d94697967e0755610
Aditya Mapari <adityamapari13@gmail.com>
2025-08-20 18:52:28 +0530

    Time: 0 ms (100%), Space: 42.4 MB (19.6%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,42 +1,36 @@
 class Solution {
     public int search(int[] nums, int target) 
     {
-       int start = 0;
-       int end = nums.length-1;
+         int start = 0;
+        int end = nums.length - 1;
 
-       while(start <= end)
-       {
-            int mid = (start + end)/2;
+        while (start <= end) 
+        {
+            int mid = (start + end) / 2;
 
-            if(nums[mid] == target)
-            {
+            if (nums[mid] == target) 
                 return mid;
-            }
 
-            else if(nums[start] <= nums[mid])
+            // Check if left half is sorted
+            if (nums[start] <= nums[mid]) 
             {
-                if(target >= nums[start] && target < nums[mid])
-                {
-                    end = mid - 1;
-                }
-                else
-                {
-                    start = mid+1;
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;  // go left
+                } else {
+                    start = mid + 1; // go right
                 }
             }
-            else
+            // Else right half must be sorted
+            else 
             {
-               if(target > nums[mid] && target <= nums[end])
-               {
-                    start = mid+1;
-               } 
-               else
-               {
-                    end = mid-1;
-               }
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1; // go right
+                } else {
+                    end = mid - 1; // go left
+                }
             }
-            
-       }
-       return -1;
+        }
+
+        return -1;
     }
 }
